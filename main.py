b9e4fdb4f839f68dfa14a561f4f9e722640e5747
3cence <benalderson725@gmail.com>
2021-06-06 23:12:20 -0400

    Made loading screen
    Needs much improvement lol

--- main.py
+++ main.py
@@ -12,15 +12,30 @@ class Window(QMainWindow):
         self.setFixedSize(960, 600)
         self.setWindowTitle("Asteroids")
         self.setWindowIcon(QIcon(resource_path("Assets/roid.png")))
+        self.label = QLabel("<font color=\"white\"> Loading Game...", self)
+        self.label.setGeometry(QRect(137, 90, 685, 121))
+        font = QFont()
+        font.setFamily(u"Yrsa")
+        font.setPointSize(80)
+        font.setBold(True)
+        font.setItalic(True)
+        font.setWeight(75)
+        self.label.setFont(font)
+        self.label.setAlignment(Qt.AlignCenter)
 
         self.gameCore = GameCore(self)
         self.startTimer = QTimer(self)
-        self.startTimer.setInterval(1000)
+        self.startTimer.setInterval(3000)
         self.startTimer.timeout.connect(self.startGame)
         self.startTimer.start()
 
     def startGame(self):
         self.gameCore.startGame()
+
+    def paintEvent(self, event):
+        pnt = QPainter(self)
+        pnt.drawPixmap(QRect(0, 0, self.gameCore.geometry().width(), self.gameCore.geometry().height()),
+                       self.gameCore.background)
 
     def keyPressEvent(self, event: QKeyEvent):
         self.gameCore.keyEventDist(event, True)
