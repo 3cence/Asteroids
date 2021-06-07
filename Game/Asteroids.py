065b095330a87857ecf215324bd9ccdb1e8536a0
3cence <benalderson725@gmail.com>
2021-06-07 12:41:56 -0400

    Asteroid particles implemented

--- Game/Asteroids.py
+++ Game/Asteroids.py
@@ -1,6 +1,7 @@
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from Utils.Resloader import resource_path
+from Utils import Particles
 import random
 
 
@@ -15,6 +16,12 @@ class Asteroids:
         self.height = 70
         self.speed = 5
 
+        #Particles
+        self.boom = Particles.Particle("Assets/spritesheets/explosion.png", columns=4, rows=4,
+                                       totalFrames=16, fps=50)
+        self.trail = Particles.Particle("Assets/spritesheets/trailing_fire.png", columns=3, rows=3,
+                                        totalFrames=8, fps=20)
+
     def tick(self, env):
         if int((random.random() * 1000) % 10) == 0:
             speed = (random.random() * 1000) % 15
@@ -25,7 +32,14 @@ class Asteroids:
 
         for aster, speed in Asteroids.asteroids:
             if env.getEarth().getSurface().intersects(QRectF(aster)):
+                Particles.spawnParticle(self.boom, aster.x(), aster.y() + 20)
                 Asteroids.asteroids.remove([aster, speed])
+
+            if int((random.random() * 1000) % 5) == 0:
+                trailScale = 2
+                Particles.spawnParticle(self.trail, aster.x() + ((aster.width() / 2) - (self.trail.jumpX * trailScale) / 2),
+                                        aster.y(), trailScale)
+
             aster.setY(aster.y() + speed)
             aster.setWidth(self.width)
             aster.setHeight(self.height)
