7e7e053800f9318f30d2d7aab52eaad520279689
3cence <benalderson725@gmail.com>
2021-06-09 00:20:31 -0400

    Added grey-out of disabled PrintBtns

--- QtOverrides/PaintBtn.py
+++ QtOverrides/PaintBtn.py
@@ -5,9 +5,9 @@ from Utils.Particles import ParticleSpritesheet
 
 
 class PaintBtn(QPushButton):
-    def __init__(self, image: str, columns: int, rows: int, frames: int, scale, parent):
+    def __init__(self, image: str, scale, parent):
         super().__init__(parent)
-        self.image = ParticleSpritesheet(image, columns, rows, frames)
+        self.image = ParticleSpritesheet(image, 1, 3, 3)
         self.setGeometry(0, 0, self.image.jumpX * scale, self.image.jumpY * scale)
         print(self.image.jumpX, self.image.jumpY)
         self.scale = scale
@@ -17,7 +17,7 @@ class PaintBtn(QPushButton):
         pnt = QPainter(self)
         self.cursorPos = QCursor().pos()
         rect = QRect(0, 0, self.width(), self.height())
-        if self.isDown():
+        if self.isDown() or not self.isEnabled():
             pnt.drawPixmap(rect, self.image.frames[2])
         elif self.hitButton(self.mapFromGlobal(self.cursorPos)):
             pnt.drawPixmap(rect, self.image.frames[1])
